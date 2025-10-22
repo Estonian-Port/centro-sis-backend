e09a82200bf171b998cb6825232719c333dbf5c2
Thaiel Anaya <thaiel.anaya@gmail.com>
2025-10-21 23:56:59 -0300

    Agrega pago

--- src/main/kotlin/com/estonianport/centro_sis/model/Pago.kt
+++ src/main/kotlin/com/estonianport/centro_sis/model/Pago.kt
@@ -12,17 +12,9 @@ import java.time.LocalDate
 
 @Entity
 class Pago(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(nullable = false)
-    var alumno: RolAlumno,
-
-    @Column(nullable = false)
-    var monto: Double,
-
-    @Column(nullable = false)
-    var fecha: LocalDate = LocalDate.now()
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0,
+        @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(nullable = false) var alumno: RolAlumno,
+        @Column(nullable = false) var monto: Double,
+        @Column(nullable = false) var fecha: LocalDate = LocalDate.now(),
+        @Column(nullable = false) val retraso: Boolean = false
 )
